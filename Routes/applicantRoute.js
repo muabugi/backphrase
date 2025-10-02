b74ba1dbd793d3faf687df529b989c03e0258ab4
muabugi <muabugi@gmail.com>
2025-10-02 01:16:23 +0100

    first commit

--- Routes/applicantRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const applicantController = require('../Controllers/applicantController');
-
-const router = express.Router();
-
-router.route('/').get(applicantController.home);
-router.route('/details').post(applicantController.emailClient_1);
-
-module.exports = router;
